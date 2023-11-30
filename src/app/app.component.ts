28c8b8a52b7335b19f7ed55eb8b8283cf0ef4ba6
nayaksofia <nayaksofia1989@gmail.com>
2023-11-30 14:27:31 +0530

    Add @Input directive : Another way of component interaction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'estore';
+  appName = 'eStore- Shop with easy like never before'
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component,OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+@Input() name = 'NA';
+
+constructor(){}
+
+ngOnInit(): void{
+
+}
 
 }
