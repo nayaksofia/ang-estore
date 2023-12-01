cdb3f209fb96d964736861ce881a3558f2f7fb4a
nayaksofia <nayaksofia1989@gmail.com>
2023-12-01 19:37:20 +0530

    Working on Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { ProductsComponent } from './products/products.component';
 
 //Create a jshon object which have path
 const routes: Routes = [
+ {path:' ', redirectTo:'/home',pathMatch:'full'},
  {path:'home', component:HomeComponent},
  {path:'products', component:ProductsComponent},
  {path:'profile', component:ProfileComponent}
